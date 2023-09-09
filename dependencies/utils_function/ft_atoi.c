e156e5a78970ab15cba7ee1c57d5ff7551d53139
SalaHmT <salah.hamsate@gmail.com>
2023-09-09 17:13:46 +0100

    create function needed by philo project

---
+++ dependencies/utils_function/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamsate <shamsate@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 16:52:36 by shamsate          #+#    #+#             */
+/*   Updated: 2023/09/09 17:11:07 by shamsate         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/philo.h"
+
+
+int	ft_atoi(const char *str)
+{
+	int sign;
+	int	i;
+	long long 	nbr;
+
+	sign = 1;
+	nbr = 0;
+	i = 0;
+
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13 ))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;	
+	}
+
+	while (str && (str[i] >= '0' && str[i] <= '9'))
+	{
+		nbr = nbr * 10 + str[i] -'0';
+        if (nbr  * sign > INT_MAX)
+            return (-1);
+        else if (nbr * sign < INT_MIN)
+            return (-1);
+		i++;
+	}
+	return((int)nbr * sign);
+}
---
+++ include/philo.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamsate <shamsate@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 16:17:54 by shamsate          #+#    #+#             */
+/*   Updated: 2023/09/09 17:11:04 by shamsate         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdint.h>
+
+// struct part :
+typedef struct philo_info
+{
+    long long   nbr_ph;
+    long long   nbr_t_eat;
+    long long   t_start;
+    long long   t_eat;
+    long long   t_sleep;
+    long long   t_die;
+    int         stop_eat;
+    bool        check;
+    pthread_mutex_t pth_lock;
+    pthread_mutex_t allow_eat;
+} philo_inf;
+
+typedef struct philo
+{
+    int ph_id;
+    int total_eat;
+    long long final_eat;
+    struct philo *next;
+    philo_inf *inf_ph;
+    pthread_mutex_t forks;
+    pthread_t ph_thrd;
+}t_ph;
+
+//libft_function 
+int	ft_atoi(const char *str);
+
+
+#endif
